1a2078e8cd130020f480778b53ba470a8ca65cc9
Hernan Saez <hhsaez@gmail.com>
2015-11-24 00:42:14 -0300

    Added flag to textures in order to support different wrap modes Includes OpenAL in iOS project

--- opengl/src/Rendering/Catalogs/TextureCatalog.cpp
+++ opengl/src/Rendering/Catalogs/TextureCatalog.cpp
@@ -114,7 +114,16 @@ void TextureCatalog::load( Texture *texture )
     glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
     glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
 
-	GLint internalFormat = GL_RGBA;
+    if ( texture->getWrapMode() == Texture::WrapMode::CLAMP_TO_EDGE ) {
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+    }
+    else if ( texture->getWrapMode() == Texture::WrapMode::REPEAT ) {
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    }
+
+    GLint internalFormat = GL_RGBA;
 	GLint format = GL_BGRA;
 	if ( texture->getImage()->getBpp() == 4 ) {
 		internalFormat = GL_RGBA;
