71859e214e2f2174695814864f3c196bc9fef877
Hernan Saez <hhsaez@gmail.com>
2017-12-18 18:58:36 -0300

    Enable comparators for testing context values in behaviors

--- core/src/Behaviors/Conditions/TestContextValue.cpp
+++ core/src/Behaviors/Conditions/TestContextValue.cpp
@@ -6,9 +6,24 @@ using namespace crimild::behaviors::conditions;
 
 TestContextValue::TestContextValue( std::string key, std::string value, std::string comparator )
 	: _key( key ),
-	  _value( value ),
-	  _comparator( comparator )
+	  _value( value )
 {
+	if ( comparator == "greater" ) {
+		_comparator = []( crimild::Real32 a, crimild::Real32 b ) -> crimild::Bool { return a > b; };
+	}
+	else if ( comparator == "gequal" ) {
+		_comparator = []( crimild::Real32 a, crimild::Real32 b ) -> crimild::Bool { return a >= b; };
+	}
+	else if ( comparator == "lequal" ) {
+		_comparator = []( crimild::Real32 a, crimild::Real32 b ) -> crimild::Bool { return a <= b; };
+	}
+	else if ( comparator == "lower" ) {
+		_comparator = []( crimild::Real32 a, crimild::Real32 b ) -> crimild::Bool { return a < b; };
+	}
+	else {
+		// no comparator uses std::string equality
+		_comparator = nullptr;
+	}
 
 }
 
@@ -19,12 +34,15 @@ TestContextValue::~TestContextValue( void )
 
 Behavior::State TestContextValue::step( BehaviorContext *context )
 {
-	auto setting = context->getValue< std::string >( _key );
-
-	if ( setting != _value ) {
-		return Behavior::State::FAILURE;
+	if ( _comparator != nullptr ) {
+		auto setting = context->getValue< crimild::Real32 >( _key );
+		crimild::Real32 v;
+		StringUtils::toValue( _value, v );
+		return ( _comparator( setting, v ) ?  Behavior::State::SUCCESS : Behavior::State::FAILURE );
 	}
 
-	return Behavior::State::SUCCESS;
+	// no comparator case
+	auto setting = context->getValue< std::string >( _key );
+	return setting == _value ? Behavior::State::SUCCESS : Behavior::State::FAILURE;
 }
 
