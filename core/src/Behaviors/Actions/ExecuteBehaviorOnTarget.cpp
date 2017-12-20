38a95c949b4c5f0d584469961a3ac23656cb90dd
Hernan Saez <hhsaez@gmail.com>
2017-12-20 16:29:32 -0300

    Added extra log

--- core/src/Behaviors/Actions/ExecuteBehaviorOnTarget.cpp
+++ core/src/Behaviors/Actions/ExecuteBehaviorOnTarget.cpp
@@ -26,15 +26,18 @@ Behavior::State ExecuteBehaviorOnTarget::step( BehaviorContext *context )
 
 	auto target = context->getTargetAt( 0 );
 	if ( target == nullptr ) {
+		Log::error( CRIMILD_CURRENT_CLASS_NAME, "Behavior requires at least one target" );
 		return Behavior::State::FAILURE;
 	}
 
 	auto behaviors = target->getComponent< BehaviorController >();
 	if ( behaviors == nullptr ) {
+		Log::error( CRIMILD_CURRENT_CLASS_NAME, "Behavior target does not have behaviors" );
 		return Behavior::State::FAILURE;
 	}
 
 	if ( !behaviors->executeBehavior( _behaviorName ) ) {
+		Log::error( CRIMILD_CURRENT_CLASS_NAME, "Cannot execute behavior on target with name ", _behaviorName );
 		return Behavior::State::FAILURE;
 	}
 	
