9d95192cd11fd1cbecfb7eb34b7e70bda0524d10
Hernan Saez <hhsaez@gmail.com>
2019-02-07 16:27:45 -0300

    UIButton and callback

---
+++ core/src/UI/UIButton.hpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2002-present, H. Hernán Saez
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CRIMILD_UI_BUTTON_
+#define CRIMILD_UI_BUTTON_
+
+#include "Components/NodeComponent.hpp"
+
+namespace crimild {
+
+	class Node;
+	
+	namespace ui {
+
+		class UIFrame;
+
+		class UIButton : public NodeComponent {
+			CRIMILD_IMPLEMENT_RTTI( crimild::ui::UIButton )
+
+		private:
+			using Callback = std::function< bool( Node * ) >;
+			
+		public:
+			UIButton( void );
+			UIButton( Callback const &callback );
+			virtual ~UIButton( void );
+
+			virtual void onAttach( void ) override;
+			virtual void start( void ) override;
+			virtual void update( const Clock & ) override;
+
+		private:
+			Callback _callback;
+			UIFrame *_frame = nullptr;
+			Node *_node = nullptr;
+		};
+
+	}
+
+}
+
+#endif
+
+
