886913b27ffd3d13ff8d2332cdb819700a3f9881
Hernan Saez <hhsaez@gmail.com>
2020-09-21 23:12:34 -0300

    Basic HDR & Tonemapping support
    * Add support for HDR targets, using RGBA32_FLOAT color attachments
    * Implement simple tonemapper based on Reinhard formula plus exposition

--- core/src/Rendering/Format.hpp
+++ core/src/Rendering/Format.hpp
@@ -32,7 +32,7 @@
 
 namespace crimild {
 
-	/**
+    /**
 	   \brief Different format values
 
 	   Suffixes:
@@ -56,6 +56,10 @@ namespace crimild {
         R8G8B8A8_UNORM,
         B8G8R8A8_UNORM,
         R16_UINT,
+        R16_SFLOAT,
+        R16G16_SFLOAT,
+        R16G16B16_SFLOAT,
+        R16G16B16A16_SFLOAT,
         R32_UINT,
         R32_SINT,
         R32_SFLOAT,
@@ -71,43 +75,48 @@ namespace crimild {
         DEPTH_24_UNORM_STENCIL_8_UINT,
         DEPTH_32_SFLOAT_STENCIL_8_UINT,
         DEPTH_STENCIL_DEVICE_OPTIMAL, //< Whatever depth/stencil format is supported
-        COLOR_SWAPCHAIN_OPTIMAL, //< Whatever format the swapchain has
+        COLOR_SWAPCHAIN_OPTIMAL,      //< Whatever format the swapchain has
         INDEX_16_UINT = R16_UINT,
         INDEX_32_UINT = R32_UINT,
     };
 
-	namespace utils {
+    namespace utils {
 
-		template< typename T >
-		static Format getFormat( void ) noexcept { return Format::UNDEFINED; }
+        template< typename T >
+        static Format getFormat( void ) noexcept { return Format::UNDEFINED; }
 
-        template<> Format getFormat< UInt16 >( void ) noexcept { return Format::R16_UINT; }
-        template<> Format getFormat< UInt32 >( void ) noexcept { return Format::R32_UINT; }
-		template<> Format getFormat< Real32 >( void ) noexcept { return Format::R32_SFLOAT; }
-		template<> Format getFormat< Vector2f >( void ) noexcept { return Format::R32G32_SFLOAT; }
-		template<> Format getFormat< Vector3f >( void ) noexcept { return Format::R32G32B32_SFLOAT; }
+        template<>
+        Format getFormat< UInt16 >( void ) noexcept { return Format::R16_UINT; }
+        template<>
+        Format getFormat< UInt32 >( void ) noexcept { return Format::R32_UINT; }
+        template<>
+        Format getFormat< Real32 >( void ) noexcept { return Format::R32_SFLOAT; }
+        template<>
+        Format getFormat< Vector2f >( void ) noexcept { return Format::R32G32_SFLOAT; }
+        template<>
+        Format getFormat< Vector3f >( void ) noexcept { return Format::R32G32B32_SFLOAT; }
 
-		static UInt32 getFormatSize( Format format ) noexcept
-		{
-			switch ( format ) {
+        static UInt32 getFormatSize( Format format ) noexcept
+        {
+            switch ( format ) {
                 case Format::R8G8B8A8_UINT:
                     return 4 * sizeof( UInt8 );
                 case Format::R16_UINT:
                     return 1 * sizeof( UInt16 );
                 case Format::R32_UINT:
                     return 1 * sizeof( UInt32 );
-				case Format::R32_SFLOAT:
-					return 1 * sizeof( Real32 );
-				case Format::R32G32_SFLOAT:
-					return 2 * sizeof( Real32 );
-				case Format::R32G32B32_SFLOAT:
-					return 3 * sizeof( Real32 );
-				default:
-					return 0;
-			};
-		}
+                case Format::R32_SFLOAT:
+                    return 1 * sizeof( Real32 );
+                case Format::R32G32_SFLOAT:
+                    return 2 * sizeof( Real32 );
+                case Format::R32G32B32_SFLOAT:
+                    return 3 * sizeof( Real32 );
+                default:
+                    return 0;
+            };
+        }
 
-	}
+    }
 
 }
 
