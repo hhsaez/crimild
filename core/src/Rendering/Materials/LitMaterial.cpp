b5d2110d9966dcbc47b875168f6cdd08a59d82b9
Hernan Saez <hhsaez@gmail.com>
2020-09-27 12:00:06 -0300

    PBR Support!
    * New LitMaterial based on PBR options like metalness and roughness
    * New LitShaderProgram implementing PBR calculations based on Disney/Unreal
    * Point and Directional lights are working. Spot is not (yet)
    * Shadows are not working (yet)
    * Shaders could use a cleanup

---
+++ core/src/Rendering/Materials/LitMaterial.cpp
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2002 - present, H. Hernan Saez
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "Rendering/Materials/LitMaterial.hpp"
+
+#include "Rendering/DescriptorSet.hpp"
+#include "Rendering/Pipeline.hpp"
+#include "Rendering/Programs/LitShaderProgram.hpp"
+#include "Rendering/UniformBuffer.hpp"
+#include "Simulation/AssetManager.hpp"
+
+using namespace crimild;
+
+LitMaterial::LitMaterial( void ) noexcept
+{
+    // Use a default pipeline
+    setPipeline(
+        [] {
+            auto pipeline = crimild::alloc< Pipeline >();
+            pipeline->program = crimild::retain( AssetManager::getInstance()->get< LitShaderProgram >() );
+            return pipeline;
+        }() );
+
+    setDescriptors(
+        [ & ] {
+            auto descriptors = crimild::alloc< DescriptorSet >();
+            descriptors->descriptors = {
+                Descriptor {
+                    .descriptorType = DescriptorType::UNIFORM_BUFFER,
+                    .obj = crimild::alloc< UniformBuffer >( Props {} ),
+                },
+                {
+                    // Albedo map
+                    .descriptorType = DescriptorType::TEXTURE,
+                    .obj = Texture::ONE,
+                },
+                {
+                    // Metallic map
+                    .descriptorType = DescriptorType::TEXTURE,
+                    .obj = Texture::ONE,
+                },
+                {
+                    // Roughness map
+                    .descriptorType = DescriptorType::TEXTURE,
+                    .obj = Texture::ONE,
+                },
+                {
+                    // Ambient Occlusion map
+                    .descriptorType = DescriptorType::TEXTURE,
+                    .obj = Texture::ONE,
+                },
+                {
+                    // Normal map
+                    .descriptorType = DescriptorType::TEXTURE,
+                    .obj = Texture::ZERO,
+                },
+            };
+            return descriptors;
+        }() );
+}
+
+LitMaterial::Props &LitMaterial::getProps( void ) noexcept
+{
+    return getDescriptors()->descriptors[ 0 ].get< UniformBuffer >()->getValue< Props >();
+}
+
+const LitMaterial::Props &LitMaterial::getProps( void ) const noexcept
+{
+    return getDescriptors()->descriptors[ 0 ].get< UniformBuffer >()->getValue< Props >();
+}
+
+void LitMaterial::setAlbedoMap( SharedPointer< Texture > const &albedoMap ) noexcept
+{
+    getDescriptors()->descriptors[ 1 ].obj = albedoMap;
+}
+
+const Texture *LitMaterial::getAlbedoMap( void ) const noexcept
+{
+    return getDescriptors()->descriptors[ 1 ].get< Texture >();
+}
+
+Texture *LitMaterial::getAlbedoMap( void ) noexcept
+{
+    return getDescriptors()->descriptors[ 1 ].get< Texture >();
+}
+
+void LitMaterial::setMetallicMap( SharedPointer< Texture > const &metallicMap ) noexcept
+{
+    getDescriptors()->descriptors[ 2 ].obj = metallicMap;
+}
+
+const Texture *LitMaterial::getMetallicMap( void ) const noexcept
+{
+    return getDescriptors()->descriptors[ 2 ].get< Texture >();
+}
+
+Texture *LitMaterial::getMetallicMap( void ) noexcept
+{
+    return getDescriptors()->descriptors[ 2 ].get< Texture >();
+}
+
+void LitMaterial::setRoughnessMap( SharedPointer< Texture > const &roughnessMap ) noexcept
+{
+    getDescriptors()->descriptors[ 3 ].obj = roughnessMap;
+}
+
+const Texture *LitMaterial::getRoughnessMap( void ) const noexcept
+{
+    return getDescriptors()->descriptors[ 3 ].get< Texture >();
+}
+
+Texture *LitMaterial::getRoughnessMap( void ) noexcept
+{
+    return getDescriptors()->descriptors[ 3 ].get< Texture >();
+}
+
+void LitMaterial::setAmbientOcclusionMap( SharedPointer< Texture > const &ambientOcclusionMap ) noexcept
+{
+    getDescriptors()->descriptors[ 4 ].obj = ambientOcclusionMap;
+}
+
+const Texture *LitMaterial::getAmbientOcclusionMap( void ) const noexcept
+{
+    return getDescriptors()->descriptors[ 4 ].get< Texture >();
+}
+
+Texture *LitMaterial::getAmbientOcclusionMap( void ) noexcept
+{
+    return getDescriptors()->descriptors[ 4 ].get< Texture >();
+}
+
+void LitMaterial::setNormalMap( SharedPointer< Texture > const &normalMap ) noexcept
+{
+    getDescriptors()->descriptors[ 5 ].obj = normalMap;
+}
+
+const Texture *LitMaterial::getNormalMap( void ) const noexcept
+{
+    return getDescriptors()->descriptors[ 5 ].get< Texture >();
+}
+
+Texture *LitMaterial::getNormalMap( void ) noexcept
+{
+    return getDescriptors()->descriptors[ 5 ].get< Texture >();
+}
---
+++ core/src/Rendering/Materials/LitMaterial.hpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2002 - present, H. Hernan Saez
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CRIMILD_CORE_RENDERING_MATERIALS_LIT_
+#define CRIMILD_CORE_RENDERING_MATERIALS_LIT_
+
+#include "Rendering/Material.hpp"
+
+namespace crimild {
+
+    /**
+       \brief A PBR-based material
+
+        Based on Disney/Unreal PBR system
+     */
+    class LitMaterial : public Material {
+    private:
+        struct Props {
+            alignas( 16 ) RGBAColorf albedo = RGBAColorf::ONE;
+            //alignas( 16 ) Real32 metallic = 0.0f;
+            //alignas( 16 ) Real32 roughness = 0.0f;
+            //alignas( 16 ) Real32 ambientOcclusion = 1.0f;
+            alignas( 16 ) RGBAColorf ops = RGBAColorf( 0.0f, 0.0f, 1.0f, 0.0f );
+        };
+
+    public:
+        LitMaterial( void ) noexcept;
+        virtual ~LitMaterial( void ) = default;
+
+        inline void setAlbedo( const RGBColorf &albedo ) noexcept { getProps().albedo = albedo.xyzw(); };
+        inline const RGBColorf &getAlbedo( void ) const noexcept { return getProps().albedo.xyz(); }
+        void setAlbedoMap( SharedPointer< Texture > const &albedoMap ) noexcept;
+        const Texture *getAlbedoMap( void ) const noexcept;
+        Texture *getAlbedoMap( void ) noexcept;
+
+        inline void setMetallic( const Real32 &metallic ) noexcept { getProps().ops[ 0 ] = metallic; };
+        inline const Real32 &getMetallic( void ) const noexcept { return getProps().ops[ 0 ]; }
+        void setMetallicMap( SharedPointer< Texture > const &metallicMap ) noexcept;
+        const Texture *getMetallicMap( void ) const noexcept;
+        Texture *getMetallicMap( void ) noexcept;
+
+        inline void setRoughness( const Real32 &roughness ) noexcept { getProps().ops[ 1 ] = roughness; };
+        inline const Real32 &getRoughness( void ) const noexcept { return getProps().ops[ 1 ]; }
+        void setRoughnessMap( SharedPointer< Texture > const &roughnessMap ) noexcept;
+        const Texture *getRoughnessMap( void ) const noexcept;
+        Texture *getRoughnessMap( void ) noexcept;
+
+        inline void setAmbientOcclusion( const Real32 &ambientOcclusion ) noexcept { getProps().ops[ 2 ] = ambientOcclusion; };
+        inline const Real32 &getAmbientOcclusion( void ) const noexcept { return getProps().ops[ 2 ]; }
+        void setAmbientOcclusionMap( SharedPointer< Texture > const &ambientOcclusionMap ) noexcept;
+        const Texture *getAmbientOcclusionMap( void ) const noexcept;
+        Texture *getAmbientOcclusionMap( void ) noexcept;
+
+        void setNormalMap( SharedPointer< Texture > const &normalMap ) noexcept override;
+        const Texture *getNormalMap( void ) const noexcept;
+        Texture *getNormalMap( void ) noexcept override;
+
+    private:
+        Props &getProps( void ) noexcept;
+        const Props &getProps( void ) const noexcept;
+    };
+
+}
+
+#endif
