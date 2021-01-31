7f0f51f574ac10ce5a05aedc8ccc6e46d72e9986
Hernan Saez <hhsaez@gmail.com>
2021-01-31 16:22:50 -0300

    Add missing header file

--- core/src/Crimild.hpp
+++ core/src/Crimild.hpp
@@ -220,6 +220,7 @@
 #include "Rendering/Compositions/DebugComposition.hpp"
 #include "Rendering/Compositions/GrayscaleComposition.hpp"
 #include "Rendering/Compositions/InvertComposition.hpp"
+#include "Rendering/Compositions/MixComposition.hpp"
 #include "Rendering/Compositions/PresentComposition.hpp"
 #include "Rendering/Compositions/RenderScene.hpp"
 #include "Rendering/Compositions/ShaderComposition.hpp"
