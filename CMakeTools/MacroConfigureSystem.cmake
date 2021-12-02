c2656ec920fbce4c00503117d68d35c40600994b
Hernan Saez <hhsaez@gmail.com>
2021-12-02 09:20:01 -0300

    Update system settings for CMake

--- CMakeTools/MacroConfigureSystem.cmake
+++ CMakeTools/MacroConfigureSystem.cmake
@@ -1,25 +1,56 @@
-# System configuration
+#
+# Copyright (c) 2002 - present, H. Hernan Saez
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holder nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
 IF ( ${CMAKE_SYSTEM_NAME} STREQUAL "Emscripten" )
   	SET( CRIMILD_ENABLE_EMSCRIPTEN ON )
 	ADD_DEFINITIONS( -DCRIMILD_PLATFORM_EMSCRIPTEN=1 )
-ELSE ()
-  	SET( CRIMILD_ENABLE_EMSCRIPTEN OFF )
+	SET( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -static -U__STRICT_ANSI__" )
+	RETURN()
 ENDIF ()
 
-IF ( APPLE )
-  	 # Enable C++17 features
-	 SET( CMAKE_CXX_FLAGS "-std=c++17 -stdlib=libc++ -U__STRICT_ANSI__ -fvisibility=hidden -fvisibility-inlines-hidden" )
+SET( CRIMILD_ENABLE_EMSCRIPTEN OFF )
 
-	 SET( CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++0x" )
-	 SET( CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++" )
+SET( CMAKE_CXX_STANDARD 20 )
+SET( CMAKE_CXX_STANDARD_REQUIRED ON )
+SET( CMAKE_CXX_EXTENSIONS OFF )
 
-	 SET( CMAKE_MACOSX_RPATH 1 )
-ELSE ( APPLE )
-  	IF ( CRIMILD_ENABLE_EMSCRIPTEN )
-		SET( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -static -U__STRICT_ANSI__" )
-	ELSE ()
-		set( CMAKE_CXX_STANDARD 20 )
-		set( CMAKE_CXX_STANDARD_REQUIRED ON )
-		set( CMAKE_CXX_EXTENSIONS OFF )
-	ENDIF ()
-ENDIF ( APPLE )
+IF ( MSVC )
+	ADD_COMPILE_OPTIONS( /W4 )
+ELSE() 
+	ADD_COMPILE_OPTIONS( 
+		-Wall 
+		-Wextra 
+		-Wpedantic 
+	)
+	
+	# Ignore specific warnings
+	ADD_COMPILE_OPTIONS( 
+		-Wno-missing-braces 
+		-Wno-c++98-compat 
+		-Wno-c++98-compat-pedantic 
+	)
+ENDIF()
