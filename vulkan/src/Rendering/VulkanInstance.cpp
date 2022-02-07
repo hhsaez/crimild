12c9d707c07536f2f93830fd687224ad86a15cfa
Hernan Saez <hernan.saez@getcruise.com>
2022-02-07 17:43:13 -0300

    Fix validation layers for latest vulkan versions

--- vulkan/src/Rendering/VulkanInstance.cpp
+++ vulkan/src/Rendering/VulkanInstance.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2002 - present, H. Hernan Saez
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the copyright holders nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -35,7 +35,7 @@ using namespace crimild::vulkan;
 VulkanInstance::~VulkanInstance( void )
 {
 	CRIMILD_LOG_TRACE( "Destroying instance" );
-	
+
 //	if ( m_renderDevice != nullptr ) {
 //		CRIMILD_LOG_TRACE( "Waiting for pending operations" );
 //		m_renderDevice->waitIdle();
