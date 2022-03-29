6f1b214e365273064dd4a25671b4f84015e2cf7d
Hernan Saez <hernan.saez@getcruise.com>
2022-03-29 09:50:28 -0300

    Allow detaching behaviors from composites

--- core/src/Behaviors/Composites/Composite.cpp
+++ core/src/Behaviors/Composites/Composite.cpp
@@ -1,52 +1,47 @@
 #include "Composite.hpp"
-#include "Coding/Encoder.hpp"
+
 #include "Coding/Decoder.hpp"
+#include "Coding/Encoder.hpp"
 
 using namespace crimild;
 using namespace crimild::behaviors;
 using namespace crimild::behaviors::composites;
 
-Composite::Composite( void )
-{
-
-}
-
-Composite::~Composite( void )
-{
-
-}
-
 void Composite::init( BehaviorContext *context )
 {
-	Behavior::init( context );
+    Behavior::init( context );
 }
 
 void Composite::attachBehavior( BehaviorPtr const &behavior )
 {
-	_behaviors.add( behavior );
+    _behaviors.add( behavior );
+}
+
+void Composite::detachBehavior( BehaviorPtr const &behavior )
+{
+    _behaviors.remove( behavior );
 }
 
 size_t Composite::getBehaviorCount( void ) const
 {
-	return _behaviors.size();
+    return _behaviors.size();
 }
 
 Behavior *Composite::getBehaviorAt( size_t index )
 {
-	return crimild::get_ptr( _behaviors[ index ] );
+    return crimild::get_ptr( _behaviors[ index ] );
 }
 
 void Composite::encode( coding::Encoder &encoder )
 {
-	Behavior::encode( encoder );
+    Behavior::encode( encoder );
 
-	encoder.encode( "behaviors", _behaviors );
+    encoder.encode( "behaviors", _behaviors );
 }
 
 void Composite::decode( coding::Decoder &decoder )
 {
-	Behavior::decode( decoder );
+    Behavior::decode( decoder );
 
-	decoder.decode( "behaviors", _behaviors );
+    decoder.decode( "behaviors", _behaviors );
 }
-
--- core/src/Behaviors/Composites/Composite.hpp
+++ core/src/Behaviors/Composites/Composite.hpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2013, Hernan Saez
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the <organization> nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -32,48 +32,48 @@
 
 namespace crimild {
 
-	namespace behaviors {
+    namespace behaviors {
 
-		namespace composites {
+        namespace composites {
 
-			/**
+            /**
 			   \brief A behavior with one or more children
 			*/
-			class Composite : public Behavior {
-				CRIMILD_IMPLEMENT_RTTI( crimild::behaviors::composites::Composite )
-				
-			protected:
-				Composite( void );
-				
-			public:
-				virtual ~Composite( void );
-				
-				virtual void init( BehaviorContext *context ) override;
-				
-				void attachBehavior( BehaviorPtr const &behavior );
-				size_t getBehaviorCount( void ) const;
-				Behavior *getBehaviorAt( size_t index );
-				
-			private:
-				Array< BehaviorPtr > _behaviors;
+            class Composite : public Behavior {
+                CRIMILD_IMPLEMENT_RTTI( crimild::behaviors::composites::Composite )
 
-				/**
+            protected:
+                Composite( void ) = default;
+
+            public:
+                virtual ~Composite( void ) = default;
+
+                virtual void init( BehaviorContext *context ) override;
+
+                void attachBehavior( BehaviorPtr const &behavior );
+                void detachBehavior( BehaviorPtr const &behavior );
+                size_t getBehaviorCount( void ) const;
+                Behavior *getBehaviorAt( size_t index );
+
+            private:
+                Array< BehaviorPtr > _behaviors;
+
+                /**
 				   \name Coding support
 				*/
-				//@{
-				
-			public:
-				virtual void encode( coding::Encoder &encoder ) override;
-				virtual void decode( coding::Decoder &decoder ) override;
-				
-				//@}
-			};
+                //@{
 
-		}
+            public:
+                virtual void encode( coding::Encoder &encoder ) override;
+                virtual void decode( coding::Decoder &decoder ) override;
 
-	}
-	
+                //@}
+            };
+
+        }
+
+    }
+
 }
 
 #endif
-
