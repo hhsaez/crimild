82bca1a9b043230be0b73a76866976c82f144722
Hernan Saez <hernan.saez@getcruise.com>
2022-06-17 19:28:16 -0300

    Implement MotionReset based on MotionState component

--- core/src/Behaviors/Actions/MotionReset.cpp
+++ core/src/Behaviors/Actions/MotionReset.cpp
@@ -1,5 +1,33 @@
+/*
+ * Copyright (c) 2002 - present, H. Hernan Saez
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "MotionReset.hpp"
 
+#include "Components/MotionStateComponent.hpp"
 #include "Mathematics/Point3_constants.hpp"
 #include "Mathematics/Transformation_apply.hpp"
 #include "Mathematics/Vector3_constants.hpp"
@@ -13,15 +41,15 @@ void MotionReset::init( BehaviorContext *context )
 {
     Behavior::init( context );
 
-    m_velocity = context->getOrCreate( "motion.velocity", Vector3::Constants::ZERO );
-    m_position = context->getOrCreate( "motion.position", Point3::Constants::ZERO );
-    m_steering = context->getOrCreate( "motion.steering", Vector3::Constants::ZERO );
+    auto agent = context->getAgent();
+    if ( agent == nullptr ) {
+        return;
+    }
 
-    context->getOrCreate( "motion.velocity.magnitude", 0.0f );
-    context->getOrCreate( "motion.velocity.max", 1.0f );
-    context->getOrCreate( "motion.maxForce", 1.0f );
-    context->getOrCreate( "motion.mass", 1.0f );
-    context->getOrCreate( "motion.slowingRadius", 1.0f );
+    m_motion = agent->getComponent< MotionState >();
+    if ( m_motion == nullptr ) {
+        m_motion = agent->attachComponent< MotionState >();
+    }
 }
 
 Behavior::State MotionReset::step( BehaviorContext *context )
@@ -32,8 +60,13 @@ Behavior::State MotionReset::step( BehaviorContext *context )
         return Behavior::State::FAILURE;
     }
 
-    m_position->get< Point3 >() = location( agent->getLocal() );
-    m_steering->get< Vector3 >() = Vector3::Constants::ZERO;
+    if ( m_motion == nullptr ) {
+        CRIMILD_LOG_WARNING( "MotionState not initialized" );
+        return Behavior::State::FAILURE;
+    }
+
+    m_motion->position = location( agent->getLocal() );
+    m_motion->steering = Vector3::Constants::ZERO;
 
     // if ( context->hasTargets() ) {
     //     auto target = context->getTargetAt( 0 );
