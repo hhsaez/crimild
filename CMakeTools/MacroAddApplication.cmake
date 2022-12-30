f1fca85a7e3268eace5bd1cd1870822c0894dbb9
Hernan Saez <hhsaez@gmail.com>
2022-12-30 12:31:00 -0300

    Remove old cmake files

--- CMakeTools/MacroAddApplication.cmake
+++
@@ -1,9 +0,0 @@
-# This macros setups a crimild-based application 
-
-MACRO( CRIMILD_ADD_APPLICATION appName srcDir )
-	SET( CRIMILD_APP_NAME ${appName} )
-	SET( CRIMILD_APP_SOURCE_DIRECTORIES ${srcDir} )
-	SET( CRIMILD_APP_INCLUDE_DIRECTORIES ${srcDir} )
-	INCLUDE( ModuleBuildApp )
-ENDMACRO( CRIMILD_ADD_APPLICATION )
-
