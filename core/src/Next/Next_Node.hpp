37c191bcb653628e431d08339ad6e24fcd00d3ec
Hernan Saez <hhsaez@gmail.com>
2025-03-18 16:51:40 -0300

    WIP

--- core/src/Next/Next_Node.hpp
+++ core/src/Next/Next_Node.hpp
@@ -56,9 +56,42 @@ namespace crimild::next {
         std::weak_ptr< Node > m_parent;
     };
 
-    class Node3D : public Node {
+    struct Transformable : public Object::Extension {
+        CRIMILD_IMPLEMENT_RTTI( crimild::next::Transformable )
+    };
+
+    class Node3D : public Node, public std::enable_shared_from_this< Node3D > {
         CRIMILD_IMPLEMENT_RTTI( crimild::next::Node3D )
     public:
+        [[nodiscard]] inline std::shared_ptr< Node3D > getParent3D( void ) const { return m_parent3D.lock(); }
+        inline void setParent3D( std::shared_ptr< Node3D > const &parent3D ) { m_parent3D = parent3D; }
+
+    private:
+        std::weak_ptr< Node3D > m_parent3D;
+    };
+
+    class Group3D : public Node3D, public std::enable_shared_from_this< Group3D > {
+        CRIMILD_IMPLEMENT_RTTI( crimild::next::Group3D )
+
+    public:
+        void attach( std::shared_ptr< Node3D > const &node )
+        {
+            m_children.push_back( node );
+            // node->setParent3D( Group3D::shared_from_this() );
+        }
+
+        auto begin( void ) const
+        {
+            return m_children.begin();
+        }
+
+        auto end( void ) const
+        {
+            return m_children.end();
+        }
+
+    private:
+        std::vector< std::shared_ptr< Node3D > > m_children;
     };
 
     class Geometry3D : public Node3D {
