c55c284323aca59e29421f5ac3e1b864cdd9854c
Hernan Saez <hhsaez@gmail.com>
2025-06-27 08:40:47 -0300

    Adding lights to an assembly

--- platforms/Editor/src/Views/Menus/NewEntityMenu.hpp
+++ platforms/Editor/src/Views/Menus/NewEntityMenu.hpp
@@ -41,6 +41,50 @@ namespace crimild::editor {
             ImGui::EndMenu();
          }
 
+         ImGui::Separator();
+
+         if ( ImGui::BeginMenu( "Light" ) ) {
+            if ( ImGui::MenuItem( "Directional" ) ) {
+               auto light = crimild::alloc< Light >( Light::Type::DIRECTIONAL );
+               light->setName( "Directional Light" );
+               light->setEnergy( 5 );
+               light->setCastShadows( true );
+               light->setLocal(
+                  lookAt(
+                     Point3f { -5, 5, 10 },
+                     Point3f { 0, 0, 0 },
+                     Vector3::Constants::UP
+                  )
+               );
+               ret = light;
+            }
+            if ( ImGui::MenuItem( "Point" ) ) {
+               auto light = crimild::alloc< Light >( Light::Type::POINT );
+               light->setName( "Point Light" );
+               light->setLocal( Transformation { .translate = { 0, 1, 0 } } );
+               ret = light;
+            }
+            if ( ImGui::MenuItem( "Spot" ) ) {
+               auto light = crimild::alloc< Light >( Light::Type::SPOT );
+               light->setName( "Spot Light" );
+               light->setColor( ColorRGB { 1.0f, 1.0f, 1.0f } );
+               light->setCastShadows( true );
+               light->setEnergy( 1000.0f );
+               light->setInnerCutoff( Numericf::DEG_TO_RAD * 20.0f );
+               light->setOuterCutoff( Numericf::DEG_TO_RAD * 25.0f );
+               light->setLocal(
+                  lookAt(
+                     Point3f { 10, 10, 0 },
+                     Point3f { 0, 0, 0 },
+                     Vector3::Constants::UP
+                  )
+               );
+               ret = light;
+            }
+
+            ImGui::EndMenu();
+         }
+
          return ret;
       }
    };
