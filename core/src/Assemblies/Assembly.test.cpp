b87744a8a81a3b06f88bd569f09b5400e2a1a49b
Hernan Saez <hhsaez@gmail.com>
2026-01-09 12:26:38 +0000

    WIP

--- core/src/Assemblies/Assembly.test.cpp
+++ core/src/Assemblies/Assembly.test.cpp
@@ -110,22 +110,99 @@ namespace crimild {
          );
          return ret;
       }
+
+      std::vector< GraphNodeId > getNeighbors( void ) const
+      {
+         std::vector< GraphNodeId > ret;
+         for ( const auto &e : edges ) {
+            ret.push_back( e.dst );
+         }
+         return ret;
+      }
    };
 
-   template< typename T >
+   template< typename T, bool GRAPH_IS_DIRECTED = true >
    struct Graph {
+
+      GraphNodeId addNode( T value )
+      {
+         const auto id = m_nextNodeId++;
+         m_nodes[ id ] = GraphNode { id, value };
+         return id;
+      }
+
+      bool hasEdge( GraphNodeId src, GraphNodeId dst ) const
+      {
+         return getEdge( src, dst ).has_value();
+      }
+
       std::optional< GraphEdge > getEdge( GraphNodeId src, GraphNodeId dst ) const
       {
-         return {};
+         if ( !m_nodes.contains( src ) || !m_nodes.contains( dst ) ) {
+            return {};
+         }
+         return m_nodes.at( src ).getEdge( dst );
+      }
+
+      std::vector< GraphEdge > getEdges( void ) const
+      {
+         std::vector< GraphEdge > ret;
+         for ( const auto &it : m_nodes ) {
+            const auto edges = it.second->getEdges();
+            for ( const auto &e : edges ) {
+               ret.push_back( e );
+            }
+         }
+         return ret;
+      }
+
+      bool addEdge( GraphNodeId src, GraphNodeId dst, float weight )
+      {
+         if ( !m_nodes.contains( src ) || !m_nodes.contains( dst ) ) {
+            // TODO: add error logging
+            return false;
+         }
+         m_nodes[ src ].addEdge( dst, weight );
+         if ( !GRAPH_IS_DIRECTED ) {
+            m_nodes[ dst ].addEdge( src, weight );
+         }
+         return true;
+      }
+
+      bool removeEdge( GraphNodeId src, GraphNodeId dst )
+      {
+         if ( !m_nodes.contains( src ) || !m_nodes.contains( dst ) ) {
+            // TODO: add error logging
+            return false;
+         }
+         m_nodes[ src ].removeEdge( dst );
+         if ( !GRAPH_IS_DIRECTED ) {
+            m_nodes[ dst ].removeEdge( src );
+         }
+         return true;
       }
 
    private:
-      std::vector< GraphNode< T > > m_nodes;
+      size_t m_nextNodeId = 0;
+      std::unordered_map< GraphNodeId, GraphNode< T > > m_nodes;
    };
 
 }
 
 TEST( Graph, test )
 {
-   EXPECT_TRUE( true );
+   crimild::Graph< int > g;
+   EXPECT_EQ( 0, g.addNode( 0 ) );
+   EXPECT_EQ( 1, g.addNode( 1 ) );
+   EXPECT_EQ( 2, g.addNode( 2 ) );
+   EXPECT_EQ( 3, g.addNode( 3 ) );
+   EXPECT_EQ( 4, g.addNode( 4 ) );
+   EXPECT_TRUE( g.addEdge( 0, 1, 1.0f ) );
+   EXPECT_TRUE( g.addEdge( 0, 3, 1.0f ) );
+   EXPECT_TRUE( g.addEdge( 0, 4, 3.0f ) );
+   EXPECT_TRUE( g.addEdge( 1, 2, 2.0f ) );
+   EXPECT_TRUE( g.addEdge( 1, 4, 1.0f ) );
+   EXPECT_TRUE( g.addEdge( 3, 4, 3.0f ) );
+   EXPECT_TRUE( g.addEdge( 4, 2, 3.0f ) );
+   EXPECT_TRUE( g.addEdge( 4, 3, 3.0f ) );
 }
