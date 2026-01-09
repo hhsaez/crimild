87d08490f6761899fdab19f22e841b31684bfaba
Hernan Saez <hhsaez@gmail.com>
2026-01-09 07:56:11 -0300

    WIP

--- core/src/Assemblies/Assembly.test.cpp
+++ core/src/Assemblies/Assembly.test.cpp
@@ -48,3 +48,84 @@ TEST( Assembly, RTTI )
 {
    EXPECT_STREQ( crimild::Assembly::__CLASS_NAME, "crimild::Assembly" );
 }
+
+namespace crimild {
+
+   using GraphNodeId = size_t;
+
+   struct GraphEdge {
+      GraphNodeId src;
+      GraphNodeId dst;
+      float weight;
+   };
+
+   template< typename T >
+   struct GraphNode {
+      GraphNodeId id;
+
+      T value;
+
+      // How can we represent a many-to-many relationships?
+      // Parallel edges
+      std::unordered_map< uint64_t, GraphEdge > edges;
+
+      size_t getEdgeCount( void ) const { return edges.size(); }
+
+      std::optional< GraphEdge > getEdge( GraphNodeId n ) const
+      {
+         if ( edges.contains( n ) ) {
+            return edges.at( n );
+         }
+         return {};
+      }
+
+      void addEdge( GraphNodeId n, float weight )
+      {
+         edges[ n ] = GraphEdge { id, n, weight };
+      }
+
+      void removeEdge( GraphNodeId n )
+      {
+         edges.erase( n );
+      }
+
+      std::vector< GraphEdge > getEdges( void ) const
+      {
+         std::vector< GraphEdge > ret;
+         for ( auto &it : edges ) {
+            ret.push_back( it.second );
+         }
+         return ret;
+      }
+
+      std::vector< GraphEdge > getSortedEdges( void ) const
+      {
+         auto ret = getEdges();
+         std::sort(
+            ret.begin(),
+            ret.end(),
+            []( auto &a, auto &b ) {
+               return a.dst < b.dst;
+            }
+         );
+         return ret;
+      }
+   };
+
+   template< typename T >
+   struct Graph {
+      std::optional< GraphEdge > getEdge( GraphNodeId src, GraphNodeId dst ) const
+      {
+         return {};
+      }
+
+   private:
+      std::vector< GraphNode< T > > m_nodes;
+   };
+
+}
+
+TEST( Graph, test )
+{
+   EXPECT_TRUE( true );
+}
